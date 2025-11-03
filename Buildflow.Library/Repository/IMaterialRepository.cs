e0bc5f9ad55a29f3b88531b95a54a3837c3de627
MADHANKUMAR1610 <madhanravirm@gmail.com>
2025-11-03 21:43:19 +0530

    material Dto,service,repository,controller created

---
+++ Buildflow.Library/Repository/IMaterialRepository.cs
@@ -0,0 +1,9 @@
+﻿using Buildflow.Utility.DTO;
+
+namespace Buildflow.Library.Repository
+{
+    public interface IMaterialRepository
+    {
+        Task<IEnumerable<MaterialDto>> GetMaterialsByProjectIdAsync(int projectId);
+    }
+}
